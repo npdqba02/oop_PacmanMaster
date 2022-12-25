17aaa5f5639d0802da924a392a457cb139c370ad
ITITIU20153 <91869178+ITITIU20153@users.noreply.github.com>
2022-12-25 18:23:43 +0700

    Add files via upload

--- Chibi-master/chibi/Model.java
+++ Chibi-master/chibi/Model.java
@@ -1,4 +1,4 @@
-package chibi;
+package pacman;
 
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -10,9 +10,7 @@ import javax.swing.JPanel;
 import javax.swing.Timer;
 
 public class Model extends JPanel implements ActionListener {
-	//Initialize model class, which handle draw main character, ghost, point ...
-	
-	private static final long serialVersionUID = 1L;
+
 	private Dimension d;
     private final Font smallFont = new Font("Arial", Font.BOLD, 14);
     private boolean inGame = false;
@@ -22,41 +20,42 @@ public class Model extends JPanel implements ActionListener {
     private final int N_BLOCKS = 15;
     private final int SCREEN_SIZE = N_BLOCKS * BLOCK_SIZE;
     private final int MAX_GHOSTS = 12;
-    private final int CHIBI_SPEED = 6;
+    private final int PACMAN_SPEED = 6;
 
     private int N_GHOSTS = 6;
     private int lives, score;
     private int[] dx, dy;
     private int[] ghost_x, ghost_y, ghost_dx, ghost_dy, ghostSpeed;
 
-    private Image heart, ghost;
-    private Image up, down, left, right;
+    private Image heart, ghost_red;
+    private Image up, down, left, right, menu, decor;
 
-    private int chibi_x, chibi_y, chibid_x, chibid_y;
+    private int pacman_x, pacman_y, pacmand_x, pacmand_y;
     private int req_dx, req_dy;
 
+    //map
     private final short levelData[] = {
-    	19, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 18, 22,
-        17, 16, 16, 16, 16, 24, 16, 16, 16, 16, 16, 16, 16, 16, 20,
-        25, 24, 24, 24, 28, 0, 17, 16, 16, 16, 16, 16, 16, 16, 20,
-        0,  0,  0,  0,  0,  0, 17, 16, 16, 16, 16, 16, 16, 16, 20,
-        19, 18, 18, 18, 18, 18, 16, 16, 16, 16, 24, 24, 24, 24, 20,
-        17, 16, 16, 16, 16, 16, 16, 16, 16, 20, 0,  0,  0,   0, 21,
-        17, 16, 16, 16, 16, 16, 16, 16, 16, 20, 0,  0,  0,   0, 21,
-        17, 16, 16, 16, 24, 16, 16, 16, 16, 20, 0,  0,  0,   0, 21,
-        17, 16, 16, 20, 0, 17, 16, 16, 16, 16, 18, 18, 18, 18, 20,
-        17, 24, 24, 28, 0, 25, 24, 24, 16, 16, 16, 16, 16, 16, 20,
-        21, 0,  0,  0,  0,  0,  0,   0, 17, 16, 16, 16, 16, 16, 20,
-        17, 18, 18, 22, 0, 19, 18, 18, 16, 16, 16, 16, 16, 16, 20,
-        17, 16, 16, 20, 0, 17, 16, 16, 16, 16, 16, 16, 16, 16, 20,
-        17, 16, 16, 20, 0, 17, 16, 16, 16, 16, 16, 16, 16, 16, 20,
-        25, 24, 24, 24, 26, 24, 24, 24, 24, 24, 24, 24, 24, 24, 28
+    	19, 26, 26, 18, 26, 26, 22, 15, 19, 26, 26, 18, 26, 26, 22,
+        21,  3,  6, 21,  3,  6, 17, 18, 20,  3,  6, 21,  3,  6, 21,
+        21,  9, 12, 21,  9, 12, 17, 16, 20,  9, 12, 21,  9, 12, 21,
+        25, 18, 18, 16, 26, 18, 16, 24, 16, 18, 26, 16, 18, 26, 20,
+         7, 17, 16, 20,  2, 17, 28,  7, 25, 20,  7, 17, 20,  7, 21,
+         5, 17, 16, 28,  5, 21, 11,  0,  14, 21,  5, 25, 20,  5, 21,
+        13, 17, 20, 11, 12, 17, 22, 13, 19, 20,  9, 14, 21, 13, 21,
+        19, 16, 16, 26, 26, 16, 24, 26, 24, 16, 26, 26, 16, 18, 20,
+        25, 16, 20, 11,  6, 21, 19, 18, 22, 21,  3, 14, 17, 24, 28,
+        14, 17, 16, 22,  4, 21, 17,  0, 20, 21,  5, 19, 20, 11, 14,
+        19, 16, 16, 20, 13, 21, 17, 24, 20, 21, 13, 17, 16, 18, 22,
+        17, 16, 16, 24, 26, 16, 16, 24, 16, 16, 26, 24, 16, 16, 20,
+        17, 16, 20, 11, 14, 17, 20, 15, 17, 20, 11, 14, 17, 16, 20,
+        17, 16, 16, 18, 18, 16, 16, 18, 16, 16, 18, 18, 16, 16, 20,
+        25, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 28
     };
 
     private final int validSpeeds[] = {1, 2, 3, 4, 6, 8};
-    private final int maxSpeed = 6;
+    private final int maxSpeed = 8;
 
-    private int currentSpeed = 3;
+    private int currentSpeed = 2;
     private short[] screenData;
     private Timer timer;
 
@@ -69,17 +68,17 @@ public class Model extends JPanel implements ActionListener {
         initGame();
     }
     
-    
     private void loadImages() {
-    	down = new ImageIcon("./Chibi-master/images/down.png").getImage();
-    	up = new ImageIcon("./Chibi-master/images/up.png").getImage();
-    	left = new ImageIcon("./Chibi-master/images/left.png").getImage();
-    	right = new ImageIcon("./Chibi-master/images/right.png").getImage();
-        ghost = new ImageIcon("./Chibi-master/images/tempo.gif").getImage();
-        heart = new ImageIcon("./Chibi-master/images/heart.png").getImage();
-
+    	down = new ImageIcon("./images/down.gif").getImage();
+    	up = new ImageIcon("./images/up.gif").getImage();
+    	left = new ImageIcon("./images/left.gif").getImage();
+    	right = new ImageIcon("./images/right.gif").getImage();
+        menu = new ImageIcon("./images/menu.gif").getImage();
+        ghost_red = new ImageIcon("./images/ghost_red.gif").getImage();
+        heart = new ImageIcon("./images/heart.png").getImage();
+        decor = new ImageIcon("./images/decor.gif").getImage();
     }
-       private void initVariables() {
+    private void initVariables() {
 
         screenData = new short[N_BLOCKS * N_BLOCKS];
         d = new Dimension(400, 400);
@@ -91,7 +90,7 @@ public class Model extends JPanel implements ActionListener {
         dx = new int[4];
         dy = new int[4];
         
-        timer = new Timer(40, this);
+        timer = new Timer(20, this);
         timer.start();
     }
 
@@ -103,18 +102,26 @@ public class Model extends JPanel implements ActionListener {
 
         } else {
 
-            moveChibi();
-            drawChibi(g2d);
+            movePacman();
+            drawPacman(g2d);
             moveGhosts(g2d);
             checkMaze();
         }
     }
-
+    
+    
     private void showIntroScreen(Graphics2D g2d) {
- 
+        Font fnt1 = new Font("arial",Font.BOLD, 15);
+        Font fnt0 = new Font("GARAMOND",Font.BOLD, 40);
+        g2d.setFont(fnt0);
+        g2d.setColor(Color.WHITE);
+        g2d.drawString("PACMAN GAME", (SCREEN_SIZE)/9, 100);
+        g2d.drawImage(menu,(SCREEN_SIZE)/6, 200, this);
     	String start = "Press SPACE to start";
+        g2d.setFont(fnt1);
         g2d.setColor(Color.yellow);
         g2d.drawString(start, (SCREEN_SIZE)/4, 150);
+        g2d.drawImage(decor,8, (SCREEN_SIZE)/3, this);
     }
 
     private void drawScore(Graphics2D g) {
@@ -232,29 +239,31 @@ public class Model extends JPanel implements ActionListener {
             ghost_y[i] = ghost_y[i] + (ghost_dy[i] * ghostSpeed[i]);
             drawGhost(g2d, ghost_x[i] + 1, ghost_y[i] + 1);
 
-            if (chibi_x > (ghost_x[i] - 12) && chibi_x < (ghost_x[i] + 12)
-                    && chibi_y > (ghost_y[i] - 12) && chibi_y < (ghost_y[i] + 12)
+            if (pacman_x > (ghost_x[i] - 12) && pacman_x < (ghost_x[i] + 12)
+                    && pacman_y > (ghost_y[i] - 12) && pacman_y < (ghost_y[i] + 12)
                     && inGame) {
 
                 dying = true;
+                 score = score -10; //if pacman dies, the score will decrecres 10 points 
             }
         }
     }
 
     private void drawGhost(Graphics2D g2d, int x, int y) {
-    	g2d.drawImage(ghost, x, y, this);
+    	g2d.drawImage(ghost_red, x, y, this);
         }
 
-    private void moveChibi() {
+    private void movePacman() {
 
         int pos;
         short ch;
 
-        if (chibi_x % BLOCK_SIZE == 0 && chibi_y % BLOCK_SIZE == 0) {
-            pos = chibi_x / BLOCK_SIZE + N_BLOCKS * (int) (chibi_y / BLOCK_SIZE);
+        if (pacman_x % BLOCK_SIZE == 0 && pacman_y % BLOCK_SIZE == 0) {
+            pos = pacman_x / BLOCK_SIZE + N_BLOCKS * (int) (pacman_y / BLOCK_SIZE);
             ch = screenData[pos];
 
-            if ((ch & 16) != 0) {
+            //increase score
+            if ((ch & 16) != 0) { //score
                 screenData[pos] = (short) (ch & 15);
                 score++;
             }
@@ -264,34 +273,34 @@ public class Model extends JPanel implements ActionListener {
                         || (req_dx == 1 && req_dy == 0 && (ch & 4) != 0)
                         || (req_dx == 0 && req_dy == -1 && (ch & 2) != 0)
                         || (req_dx == 0 && req_dy == 1 && (ch & 8) != 0))) {
-                    chibid_x = req_dx;
-                    chibid_y = req_dy;
+                    pacmand_x = req_dx;
+                    pacmand_y = req_dy;
                 }
             }
 
             // Check for standstill
-            if ((chibid_x == -1 && chibid_y == 0 && (ch & 1) != 0)
-                    || (chibid_x == 1 && chibid_y == 0 && (ch & 4) != 0)
-                    || (chibid_x == 0 && chibid_y == -1 && (ch & 2) != 0)
-                    || (chibid_x == 0 && chibid_y == 1 && (ch & 8) != 0)) {
-                chibid_x = 0;
-                chibid_y = 0;
+            if ((pacmand_x == -1 && pacmand_y == 0 && (ch & 1) != 0)
+                    || (pacmand_x == 1 && pacmand_y == 0 && (ch & 4) != 0)
+                    || (pacmand_x == 0 && pacmand_y == -1 && (ch & 2) != 0)
+                    || (pacmand_x == 0 && pacmand_y == 1 && (ch & 8) != 0)) {
+                pacmand_x = 0;
+                pacmand_y = 0;
             }
         } 
-        chibi_x = chibi_x + CHIBI_SPEED * chibid_x;
-        chibi_y = chibi_y + CHIBI_SPEED * chibi_y;
+        pacman_x = pacman_x + PACMAN_SPEED * pacmand_x;
+        pacman_y = pacman_y + PACMAN_SPEED * pacmand_y;
     }
 
-    private void drawChibi(Graphics2D g2d) {
+    private void drawPacman(Graphics2D g2d) {
 
         if (req_dx == -1) {
-        	g2d.drawImage(left, chibi_x + 1, chibi_y + 1, this);
+        	g2d.drawImage(left, pacman_x + 1, pacman_y + 1, this);
         } else if (req_dx == 1) {
-        	g2d.drawImage(right, chibi_x + 1, chibi_y + 1, this);
+        	g2d.drawImage(right, pacman_x + 1, pacman_y + 1, this);
         } else if (req_dy == -1) {
-        	g2d.drawImage(up, chibi_x + 1, chibi_y + 1, this);
+        	g2d.drawImage(up, pacman_x + 1, pacman_y + 1, this);
         } else {
-        	g2d.drawImage(down, chibi_x + 1, chibi_y + 1, this);
+        	g2d.drawImage(down, pacman_x + 1, pacman_y + 1, this);
         }
     }
 
@@ -303,7 +312,7 @@ public class Model extends JPanel implements ActionListener {
         for (y = 0; y < SCREEN_SIZE; y += BLOCK_SIZE) {
             for (x = 0; x < SCREEN_SIZE; x += BLOCK_SIZE) {
 
-                g2d.setColor(new Color(0,72,251));
+                g2d.setColor(new Color(19,53,191));
                 g2d.setStroke(new BasicStroke(5));
                 
                 if ((levelData[i] == 0)) { 
@@ -332,7 +341,6 @@ public class Model extends JPanel implements ActionListener {
                     g2d.setColor(new Color(255,255,255));
                     g2d.fillOval(x + 10, y + 10, 6, 6);
                }
-
                 i++;
             }
         }
@@ -344,7 +352,7 @@ public class Model extends JPanel implements ActionListener {
         score = 0;
         initLevel();
         N_GHOSTS = 6;
-        currentSpeed = 3;
+        currentSpeed = 1;
     }
 
     private void initLevel() {
@@ -364,8 +372,8 @@ public class Model extends JPanel implements ActionListener {
 
         for (int i = 0; i < N_GHOSTS; i++) {
 
-            ghost_y[i] = 4 * BLOCK_SIZE; //start position
-            ghost_x[i] = 4 * BLOCK_SIZE;
+            ghost_y[i] = 10 * BLOCK_SIZE; //start position
+            ghost_x[i] = 6 * BLOCK_SIZE;
             ghost_dy[i] = 0;
             ghost_dx[i] = dx;
             dx = -dx;
@@ -378,10 +386,10 @@ public class Model extends JPanel implements ActionListener {
             ghostSpeed[i] = validSpeeds[random];
         }
 
-        chibi_x = 7 * BLOCK_SIZE;  //start position
-        chibi_y = 11 * BLOCK_SIZE;
-        chibi_x = 0;	//reset direction move
-        chibi_y = 0;
+        pacman_x = 7 * BLOCK_SIZE;  //start position
+        pacman_y = 7 * BLOCK_SIZE;
+        pacmand_x = 0;	//reset direction move
+        pacmand_y = 0;
         req_dx = 0;		// reset direction controls
         req_dy = 0;
         dying = false;
@@ -390,7 +398,7 @@ public class Model extends JPanel implements ActionListener {
  
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-
+        //create screen
         Graphics2D g2d = (Graphics2D) g;
 
         g2d.setColor(Color.black);
@@ -401,7 +409,10 @@ public class Model extends JPanel implements ActionListener {
 
         if (inGame) {
             playGame(g2d);
-        } else {
+        }else {
+            g2d.setColor(Color.black);
+            g2d.fillRect(0, 0, d.width, d.height);
+            drawMaze(g2d);
             showIntroScreen(g2d);
         }
 
@@ -417,7 +428,6 @@ public class Model extends JPanel implements ActionListener {
         public void keyPressed(KeyEvent e) {
 
             int key = e.getKeyCode();
-
             if (inGame) {
                 if (key == KeyEvent.VK_LEFT) {
                     req_dx = -1;
