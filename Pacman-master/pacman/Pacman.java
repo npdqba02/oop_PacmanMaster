887455d858863e0d98e7d32c852b864288e77b80
Codeing <qtuong010@gmail.com>
2022-12-30 15:58:35 +0700

    Update Pacman.java

--- Pacman-master/pacman/Pacman.java
+++ Pacman-master/pacman/Pacman.java
@@ -1,18 +1,17 @@
-package pacmen;
+package pacman;
+
 import javax.swing.JFrame;
 import menu.Menu;
 
-
-public class Pacman extends JFrame{
+public class Pacman extends JFrame {
 
 	public Pacman() {
 		add(new Model());
 	}
-	
-	
+
 	public static void main(String[] args) {
 		Menu menu = new Menu();
-		
+
 	}
 
 }
